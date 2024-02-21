64da1ef1c1781af35eae44574afcbb0571820f72
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-02-21 14:32:28 +0100

    feat: sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,11 +12,11 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
 			<Sidebar />
 			<div className='pl-[60px] h-full'>
 				<div className='flex gap-x-3 h-full'>
-                    <OrgSidebar />
+					<OrgSidebar />
 					<div className='h-full flex-1'>
 						<Navbar />
 						{children}
-					oO</div>
+					</div>
 				</div>
 			</div>
 		</main>
--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -8,8 +8,8 @@ export const Loading = () => {
 				alt='Logo'
 				width={120}
 				height={120}
-				className='animate-pulse duration-700'
-                priority
+				className='animate-pulse duration-700 w-[120px] h-[120px]'
+				priority
 			/>
 		</div>
 	);
