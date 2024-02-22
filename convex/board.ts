2bdce52dadf8b111ba12e842617dab4b4ffb82e4
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-02-22 19:21:33 +0100

    feat: search querying

--- convex/board.ts
+++ convex/board.ts
@@ -49,7 +49,18 @@ export const remove = mutation({
 			throw new Error('Unauthorized');
 		}
 
-		// TODO: check to delete favorite relation as well
+		const userId = identity.subject;
+
+		const existingFavorite = await ctx.db
+			.query('userFavorites')
+			.withIndex('by_user_board', (q) =>
+				q.eq('userId', userId).eq('boardId', args.id)
+			)
+			.unique();
+
+		if (existingFavorite) {
+			await ctx.db.delete(existingFavorite._id);
+		}
 
 		await ctx.db.delete(args.id);
 	},
@@ -102,8 +113,8 @@ export const favorite = mutation({
 
 		const existingFavorite = await ctx.db
 			.query('userFavorites')
-			.withIndex('by_user_board_org', (q) =>
-				q.eq('userId', userId).eq('boardId', board._id).eq('orgId', args.orgId)
+			.withIndex('by_user_board', (q) =>
+				q.eq('userId', userId).eq('boardId', board._id)
 			)
 			.unique();
 
