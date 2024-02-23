6c56e8ee7da51ff7fd41f045096a4c46bf761a6a
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-02-23 10:30:59 +0100

    feat: liveblocks setup

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -7,9 +7,15 @@ export const Toolbar = () => {
 				<div>Circle</div>
 			</div>
 			<div className='bg-white rounded-md p-1.5 flex flex-col items-center shadow-md'>
-                <div>Undo</div>
-                <div>Redo</div>
-            </div>
+				<div>Undo</div>
+				<div>Redo</div>
+			</div>
 		</div>
+	);
+};
+
+Toolbar.Skeleton = function ToolbarSkeleton() {
+	return (
+		<div className='absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md' />
 	);
 };
