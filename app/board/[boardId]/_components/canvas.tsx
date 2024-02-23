ce9feb9ebb2d9409f687a694f63aa782bce47305
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-02-23 11:19:55 +0100

    feat: room auth

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,11 +4,15 @@ import { Info } from './info';
 import { Participants } from './participants';
 import { Toolbar } from './toolbar';
 
+import { useSelf } from '@/liveblocks.config';
+
 interface CanvasProps {
 	boardId: string;
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+    const info = useSelf((me) => me.info);
+    
 	return (
 		<main className='h-full w-full relative bg-neutral-100 touch-none'>
 			<Info />
--- convex/board.ts
+++ convex/board.ts
@@ -1,5 +1,5 @@
 import { v } from 'convex/values';
-import { mutation } from './_generated/server';
+import { mutation, query } from './_generated/server';
 
 const images = [
 	'/placeholders/1.svg',
@@ -161,6 +161,17 @@ export const unfavorite = mutation({
 		}
 
 		await ctx.db.delete(existingFavorite._id);
+
+		return board;
+	},
+});
+
+export const get = query({
+	args: {
+		id: v.id('boards'),
+	},
+	handler: async (ctx, args) => {
+		const board = ctx.db.get(args.id);
 
 		return board;
 	},
