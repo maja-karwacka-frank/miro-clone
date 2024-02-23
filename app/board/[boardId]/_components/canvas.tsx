a8182bf6510c4e63e74e7c90aaabb1d3d97b52c9
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-02-23 11:59:28 +0100

    feat: room info

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,18 +4,14 @@ import { Info } from './info';
 import { Participants } from './participants';
 import { Toolbar } from './toolbar';
 
-import { useSelf } from '@/liveblocks.config';
-
 interface CanvasProps {
 	boardId: string;
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
-    const info = useSelf((me) => me.info);
-    
 	return (
 		<main className='h-full w-full relative bg-neutral-100 touch-none'>
-			<Info />
+			<Info boardId={boardId} />
 			<Participants />
 			<Toolbar />
 		</main>
