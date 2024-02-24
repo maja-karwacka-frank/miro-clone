294a3f5d526fe4afd7ac1374a164de5888340a88
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-02-24 15:00:35 +0100

    feat: insert layer

--- components/room.tsx
+++ components/room.tsx
@@ -2,8 +2,11 @@
 
 import { ReactNode } from 'react';
 import { ClientSideSuspense } from '@liveblocks/react';
+import { LiveMap, LiveList, LiveObject } from '@liveblocks/client';
 
+import { Layer } from '@/types/canvas';
 import { RoomProvider } from '@/liveblocks.config';
+import { Layers } from 'lucide-react';
 
 interface RoomProps {
 	children: ReactNode;
@@ -17,6 +20,11 @@ export const Room = ({ children, roomId, fallback }: RoomProps) => {
 			id={roomId}
 			initialPresence={{
 				cursor: null,
+				selection: [],
+			}}
+			initialStorage={{
+				layers: new LiveMap<string, LiveObject<Layer>>(),
+				layerIds: new LiveList(),
 			}}>
 			<ClientSideSuspense fallback={fallback}>
 				{() => children}
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,5 +1,12 @@
-import { createClient } from '@liveblocks/client';
+import {
+	createClient,
+	LiveList,
+	LiveMap,
+	LiveObject,
+} from '@liveblocks/client';
 import { createRoomContext, createLiveblocksContext } from '@liveblocks/react';
+
+import { Layer } from '@/types/canvas';
 
 const client = createClient({
 	throttle: 16,
@@ -51,8 +58,8 @@ const client = createClient({
 // and that will automatically be kept in sync. Accessible through the
 // `user.presence` property. Must be JSON-serializable.
 type Presence = {
-	cursor: { x: number, y: number } | null,
-	// ...
+	cursor: { x: number; y: number } | null;
+	selection: string[];
 };
 
 // Optionally, Storage represents the shared document that persists in the
@@ -60,8 +67,8 @@ type Presence = {
 // LiveList, LiveMap, LiveObject instances, for which updates are
 // automatically persisted and synced to all connected clients.
 type Storage = {
-	// author: LiveObject<{ firstName: string, lastName: string }>,
-	// ...
+	layers: LiveMap<string, LiveObject<Layer>>;
+	layerIds: LiveList<string>;
 };
 
 // Optionally, UserMeta represents static/readonly metadata on each user, as
