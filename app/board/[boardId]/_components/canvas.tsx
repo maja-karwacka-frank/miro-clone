df13d528a4c57c873272aa8021e94fc69823e27e
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-02-26 09:32:54 +0100

    feat: color change & deletion

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -32,6 +32,7 @@ import {
 import { LiveObject } from '@liveblocks/client';
 import { LayerPreview } from './layer-preview';
 import { SelectionBox } from './selection-box';
+import { SelectionTools } from './selection-tools';
 
 const MAX_LAYERS = 100;
 
@@ -273,6 +274,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 				undo={history.undo}
 				redo={history.redo}
 			/>
+			<SelectionTools camera={camera} setLastUsedColor={setLastUsedColor} />
 			<svg
 				className='h-[100vh] w-[100vw]'
 				onWheel={onWheel}
---
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { useSelectionBounds } from '@/hooks/use-selection-bounce';
+import { useMutation, useSelf } from '@/liveblocks.config';
+import { Camera, Color } from '@/types/canvas';
+import { Trash2 } from 'lucide-react';
+import { memo } from 'react';
+import { ColorPicker } from './color-picker';
+import { useDeleteLayers } from '@/hooks/use-delete-layers';
+import { Button } from '@/components/ui/button';
+import { Hint } from '@/components/hint';
+
+interface SelectionToolsProps {
+	camera: Camera;
+	setLastUsedColor: (color: Color) => void;
+}
+
+export const SelectionTools = memo(
+	({ camera, setLastUsedColor }: SelectionToolsProps) => {
+		const selection = useSelf((me) => me.presence.selection);
+
+		const setFill = useMutation(
+			({ storage }, fill: Color) => {
+				const liveLayers = storage.get('layers');
+				setLastUsedColor(fill);
+
+				selection.forEach((id) => {
+					liveLayers.get(id)?.set('fill', fill);
+				});
+			},
+			[selection, setLastUsedColor]
+		);
+
+		const deleteLayers = useDeleteLayers();
+
+		const selectionBounds = useSelectionBounds();
+
+		if (!selectionBounds) return;
+
+		const x = selectionBounds.width / 2 + selectionBounds.x + camera.x;
+		const y = selectionBounds.y + camera.x;
+		return (
+			<div
+				className='absolute p-3 rounded-xl bg-white shadow-sm border flex select-none'
+				style={{
+					transform: `translate(
+                    calc(${x}px - 50%),
+                    calc(${y - 16}px - 100%)
+                )`,
+				}}>
+				<ColorPicker onChange={setFill} />
+				<div className='flex items-center pl-2 ml-2 border-l border-neutral-200'>
+					<Hint label='Delete'>
+						<Button variant='board' size='icon' onClick={deleteLayers}>
+							<Trash2 />
+						</Button>
+					</Hint>
+				</div>
+			</div>
+		);
+	}
+);
+
+SelectionTools.displayName = 'SelectionTools';
