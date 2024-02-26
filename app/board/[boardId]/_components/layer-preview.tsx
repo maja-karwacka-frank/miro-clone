adf10b94f12bf7c2325b95b1d34675151fdb4f66
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-02-26 11:29:06 +0100

    feat: note, ellipse elements

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -6,6 +6,9 @@ import { LayerType } from '@/types/canvas';
 import { useStorage } from '@/liveblocks.config';
 
 import { Rectangle } from './rectangle';
+import { Ellipse } from './ellipse';
+import { Text } from './text';
+import { Note } from './note';
 
 interface LayerPreviewProps {
 	id: string;
@@ -20,6 +23,33 @@ export const LayerPreview = memo(
 		if (!layer) return null;
 
 		switch (layer.type) {
+			case LayerType.Note:
+				return (
+					<Note
+						id={id}
+						layer={layer}
+						onPointerDown={onLayerPointerDown}
+						selectionColor={selectionColor}
+					/>
+				);
+			case LayerType.Text:
+				return (
+					<Text
+						id={id}
+						layer={layer}
+						onPointerDown={onLayerPointerDown}
+						selectionColor={selectionColor}
+					/>
+				);
+			case LayerType.Ellipse:
+				return (
+					<Ellipse
+						id={id}
+						layer={layer}
+						onPointerDown={onLayerPointerDown}
+						selectionColor={selectionColor}
+					/>
+				);
 			case LayerType.Rectangle:
 				return (
 					<Rectangle
